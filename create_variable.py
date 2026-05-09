171d8846731d89610a4d8589bdb380ef8c2da8fe
chakravarthi gadiparthi <chakravarthi.450@gmail.com>
2026-05-09 13:22:34 +0100

    create a program file that assign the input to the variable , then finding the length of the variable using the length function

---
+++ create_variable.py
@@ -0,0 +1,6 @@
+# create a variable and assign the input value to it , 
+# and find the length of the input , 
+# then print the length
+username = input("enter your username: ")
+length = len(username)
+print(length)
