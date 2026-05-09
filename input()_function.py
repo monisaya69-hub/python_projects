ebc2320a754c7d4b7614cc794fd669c42a9e8561
chakravarthi gadiparthi <chakravarthi.450@gmail.com>
2026-05-09 13:04:10 +0100

    input() function that allows to take user input in python and prints it with hello and ! using concatination

---
+++ input()_function.py
@@ -0,0 +1,2 @@
+# The input() function allows you to take user input in Python. It prompts the user to enter some data and returns it as a string. In this example, we are asking the user to enter their name and then greeting them with a personalized message.
+print("hello " + input("Enter your name: ") + "!")
