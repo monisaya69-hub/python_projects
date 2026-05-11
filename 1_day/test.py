2bdbe812db0bd285e008ac1c7c758f18a2717a8a
chakravarthi gadiparthi <chakravarthi.450@gmail.com>
2026-05-11 20:29:30 +0100

    round function

--- 1_day/test.py
+++ 1_day/test.py
@@ -1,4 +1,4 @@
-# print(type("mounika"))
-# print(type(True))
-float("123")
-print(float("123"))
+height = 1.65
+weight = 80
+bmi = weight / height ** 2
+print(round(3.4999999999999999))
---
+++ 2_day/round_operator.py
@@ -0,0 +1,4 @@
+mounika_wght=59.859
+print(round(mounika_wght)) # print the weight rounded to the nearest whole number
+print(round(mounika_wght, 2)) #print the weight with 2 decimal places
+print(int(mounika_wght)) # print the weight as an integer (without decimal places)
