9287263a1b312f3173bbfcc610a4f50b740b0bfe
chakravarthi gadiparthi <chakravarthi.450@gmail.com>
2026-05-12 01:32:45 +0100

    day_3 project for controlflow if else statement

---
+++ day_3/project_controlflowcondition.py
@@ -0,0 +1,9 @@
+print("welcome to the rolercoster")
+
+height = int(input("please enter your height"))
+
+if height > 120 :
+    print("you can ride the rolarcoster")
+else:
+    print("you have to grow heigh enough to ride the rolarcoster")
+    
